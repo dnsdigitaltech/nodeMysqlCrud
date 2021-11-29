24969a613a741a2366e8f24d0739b57077b416e8
Davi Bernardo <dnssites@gmail.com>
2021-11-29 13:46:36 -0300

    Pegando todos os dados da base

--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -1,4 +1,11 @@
 const db = require('../db');
 module.exports = {
-    
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitato) => {
+            db.query('SELECT * FROM carros', (error, results) => {
+                if(error) {rejeitato(error); return;}
+                aceito(results);
+            });
+        })
+    }
 }
