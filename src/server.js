53ab5f18c79bc29a4a61e6e75d9934f412a45b4a
Davi Bernardo <dnssites@gmail.com>
2021-11-29 12:14:22 -0300

    Configuraçao do servidor

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config({path:'variaveis.env'});
+const express = require('express');
+const cors = require('cors');
+const bobyParser = require('body-parser');
+
+const routes = require('./routes');
+
+const server = express();
+server.use(cors());
+server.use(bobyParser.urlencoded({extends:false}));
+server.listen(process.env.PORT, ()=>{
+    console.log(`Servidor rodando em: http://localhot:${process.env.PORT}`)
+})
